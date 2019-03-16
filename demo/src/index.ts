49eab070a30d85cd6476a9bb0dbe0442f2f6fb13
Henning Dieterichs <henning.dieterichs@live.de>
2019-03-16 15:36:23 +0100

    update demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -3,33 +3,11 @@ import {
 	types as t,
 	requestContract,
 	notificationContract,
-	StreamBasedChannel,
-	TypedChannel,
-	StreamLogger,
-	MessageStream,
 	ConsoleRpcLogger,
-	RequestContract,
-	ContractObject,
-	Contract,
-	NotificationContract,
 	ConsoleStreamLogger,
-	AnyRequestContract,
-	AsOneSideContract,
-	RuntimeJsonTypeArrOrObj,
-	RuntimeJsonType,
-	JSONValue,
-	AsRequestContract,
-	OneSideContract,
-	JSONArray,
-	JSONObject,
 } from "@hediet/typed-json-rpc";
 import { WebSocketStream } from "@hediet/typed-json-rpc-websocket";
 import { startWebSocketServer } from "@hediet/typed-json-rpc-websocket-server";
-import * as winston from "winston";
-
-const logger2 = winston.createLogger({
-	transports: [new winston.transports.Console({})],
-});
 
 const c = contract({
 	server: {
@@ -42,15 +20,12 @@ const c = contract({
 	},
 });
 
-//type MapRequest<TRequest extends >
-
 const clients = new Set<typeof c.TClientInterface>();
 
 const logger = new ConsoleRpcLogger();
 
-startWebSocketServer({ port: 12345 }, logger, channel => {
-	logger2.info("client");
-	const client = c.registerServer(channel, {
+startWebSocketServer({ port: 12345 }, stream => {
+	const { client } = c.registerServerToStream(stream, logger, {
 		sendMessage: async (args, {}) => {
 			console.log(args.msg);
 			for (const c of clients) {
@@ -59,23 +34,19 @@ startWebSocketServer({ port: 12345 }, logger, channel => {
 		},
 	});
 	clients.add(client);
-
-	channel.startListen();
 });
 
-async function foo() {
-	const channel = TypedChannel.fromStream(
+async function main() {
+	const { server } = c.getServerFromStream(
 		new ConsoleStreamLogger(
 			await WebSocketStream.connectTo({ address: "ws://localhost:12345" })
 		),
-		logger
+		logger,
+		{
+			onNewMessage: (args, {}) => console.log("onNewMessage: ", args.msg),
+		}
 	);
-	const server = c.getServer(channel, {
-		onNewMessage: (args, {}) => logger2.info("onNewMessage: ", args.msg),
-	});
-	channel.startListen();
-
 	server.sendMessage({ msg: "hellow world" });
 }
 
-foo();
+main();
